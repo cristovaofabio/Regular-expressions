d65eb9899c0b56335682f2a27694773611085502
Cristovao <cristovaofabio3@gmail.com>
2022-05-25 11:14:32 -0300

    ip and cpf exercice

--- base.js
+++ base.js
@@ -10,7 +10,9 @@ const files = [
 ];
 const html = '<h1>I am a title</h1> <p>Hello, world!</p> <p>Hello again</p>'
 const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz 01234 56789';
+const cpfNumbers = 'The CPFs are: 000.000.000-00 111.111.111-11 999.999.999-99 555.555.555-55';
+const ips = 'the ips are: 0.0.0.0 192.168.0.1 10.10.5.12 255.255.255.255';
 
 module.exports = {
-    text, files, html, alphabet
+    text, files, html, alphabet, cpfNumbers, ips
 }
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { text, files, html, alphabet } = require('./base');
+const { text, files, html, alphabet, cpfNumbers, ips } = require('./base');
 
 const regExp = /(Mike )(IS LEARNING)/gi;
 const regOrExp = /Mike|JavaScript/gi;
@@ -42,3 +42,35 @@ console.log(alphabet.match(/[^abc123]/g)); //only exclude abc and 123
 console.log(alphabet.match(/[0-9]/g)); //separate elements
 console.log(alphabet.match(/[0-9]+/g)); //groups of elements
 console.log(alphabet.match(/[a-zA-Z0-9]+/g)); //groups of elements
+
+console.log('------CPF numbers------');
+console.log(cpfNumbers.match(/[\d.-]+/g));
+/* 
+    two groups of three digits and a point, 
+    one group of three digits and one line, 
+    and one group of the two digits: 
+*/
+console.log(cpfNumbers.match(/(\d{3}\.){2}\d{3}\-\d{2}/g));
+
+console.log('------IPs------');
+console.log(ips.match(/[\d.]+/g));
+
+// 25[0-5] = number between 250 and 255
+// 2[0-4][0-9] = number between 200 and 249
+// 1\d{2} = numbers between 100 and 199
+// [1-9]\d = numbers between 10 and 99
+// \d = numbers between 0 and 9
+const ipRegExp = /((25[0-5]|2[0-4][0-9]|1\d{2}|[1-9]\d|\d)(\.)){3}(25[0-5]|2[0-4][0-9]|1\d{2}|[1-9]\d|\d)/g; // number: 250 -255
+console.log(ips.match(ipRegExp));
+
+/*
+for (let i = 0; i < 300; i++) {
+    const ip = `${i}.${i}.${i}.${i}`;
+
+    if (ip.match(ipRegExp)) {
+        console.log(`----- ${ip.match(ipRegExp)}`);
+    } else {
+        console.log(ip);
+    }
+}
+*/
